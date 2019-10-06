70f706f1a53a680c5f73a68865d332f3250e822d
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2019-10-06 16:19:05 +0100

    Created the command handler and a command that lists all commands that have the visible property set to true.

--- app.js
+++ app.js
@@ -2,11 +2,72 @@
 const config = require("./config.json");
 
 // Package Dependencies
+const fs = require("fs");
 const Discord = require("discord.js");
 
 // Setup the Discord Client
 const client = new Discord.Client({
 	fetchAllMembers: true
+});
+
+// Create an object to store the command in.
+var commands = {};
+
+// Run the command handler when the bot is ready.
+client.on("ready", () => {
+	fs.readdir(config.commands_directory, (error, files) => {
+		for (const file of files) {
+			commands[file.replace(".js", "")] = require(`${config.commands_directory}/${file}`);
+			console.log(`+ ${file}`);
+		}
+	});
+});
+
+// Run when a message is sent
+client.on("message", (message) => {
+	for (const commandFile in commands) {
+		const command = commands[commandFile];
+		const properties = command.properties;
+		const args = message.content.split(" ");
+
+		console.log("message sent", message.author.id);
+		if (args[0].replace(config.prefix, "") == properties.command) {
+			if (properties.prefix) {
+				if (message.content.startsWith(config.prefix)) {
+					if (properties.arguments.length == 0) {
+						command.run(message);
+					} else {
+						command.run(message, args);
+					}
+				}	
+			} else {
+				if (message.content.startsWith(properties.command)) {
+					if (properties.arguments.length == 0) {
+						command.run(message);
+					} else {
+						command.run(message, args);
+					}
+				}	
+			}
+		}
+	}
+
+	if (message.content.startsWith(`${config.prefix}commands`)) {
+		const embed = new Discord.RichEmbed();
+
+		embed.setTitle("Commands");
+		for (const commandFile in commands) {
+			const properties = commands[commandFile].properties;
+
+			if (properties.visible) {
+				const command = properties.prefix ? `${config.prefix}${properties.command}` : properties.command;
+				const arguments = properties.arguments.length == 0 ? "" : properties.arguments.toString().replace(new RegExp(",", "g"), " ");
+
+				embed.addField(`${command} ${arguments}`, properties.description, true);
+			}
+		}
+		message.channel.send({embed});
+	}
 });
 
 
