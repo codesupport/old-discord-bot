81ddae8188163d4f53db45c077ed31e37be03f11
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2019-11-01 22:11:45 +0000

    Added a check to see if the Discord account is over 30 days old.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,7 +5,12 @@ const config = app.config;
 
 // The code that runs when the event is executed.
 function run(member) {
-	if (member.user.avatar) {
+	const currentDate = Date.now();
+	const memberCreatedDate = member.user.createdAt.getTime();
+	const dateDifference = currentDate - memberCreatedDate;
+
+	// Check that the member has an avatar and their account is at least 30 days old.
+	if (member.user.avatar && dateDifference / 2592000000 > 1) {
 		member.addRole(config.roles.member, "Appears to be a valid account.");
 	}
 }
