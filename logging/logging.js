426440a337b0901a62ef67dfffe2bc24433280cf
Atrum <kyle@design-wright.net>
2019-12-10 11:26:31 -0500

    Moved functions into pseudo class

--- logging/logging.js
+++ logging/logging.js
@@ -23,207 +23,210 @@ const DateUtils = require("../utils/dateUtils.js");
 const StringUtils = require("../utils/stringUtils.js");
 const Terminal = require("./terminal.js");
 
+const Logger = (() => {
 /*
  * Color pallet used for setting log level colours.
  */
-const colorPallet = {
-	error: Terminal.color.red,
-	warn: Terminal.color.yellow,
-	info: Terminal.color.cyan,
-	debug: Terminal.color.green
-};
+	const colorPallet = {
+		error: Terminal.color.red,
+		warn: Terminal.color.yellow,
+		info: Terminal.color.cyan,
+		debug: Terminal.color.green
+	};
 
-const logLevels = ["DEBUG", "INFO", "WARN", "ERROR"];
-const logLevelIndex = {"DEBUG": 0, "INFO": 1, "WARN": 2, "ERROR": 3};
-let loggerLogLevel = "DEBUG";
-let loggerPattern = "[%d{Y-m-d H:i:s.S}] | %-5l | %s";
+	const logLevels = ["DEBUG", "INFO", "WARN", "ERROR"];
+	const logLevelIndex = {"DEBUG": 0, "INFO": 1, "WARN": 2, "ERROR": 3};
+	let loggerLogLevel = "DEBUG";
+	let loggerPattern = "[%d{Y-m-d H:i:s.S}] | %-5l | %s";
 
-/*
- * Replaces the date placeholder in the passed format.
- *
- * Parameters:
- * logFormat - The log format string to parse.
- */
-function formatDate(logFormat) {
-	let logValue = logFormat;
-	const matcher = StringUtils.match("%d\{(.*)\}", logFormat);
+	/*
+	 * Replaces the date placeholder in the passed format.
+	 *
+	 * Parameters:
+	 * logFormat - The log format string to parse.
+	 */
+	function formatDate(logFormat) {
+		let logValue = logFormat;
+		const matcher = StringUtils.match("%d\{(.*)\}", logFormat);
 
-	if (matcher.isMatch === true) {
-		const dateFormatRegex = matcher.match[0];
-		const dateFormat = matcher.match[1];
+		if (matcher.isMatch === true) {
+			const dateFormatRegex = matcher.match[0];
+			const dateFormat = matcher.match[1];
 
-		logValue = logValue.replace(dateFormatRegex, DateUtils.format(dateFormat, new Date()));
-	}
-	return logValue;
-}
-
-/*
- * Replaces the log level placeholder in the passed format.
- *
- * Parameters:
- * logFormat - The log format string to parse.
- * level - The log level to inject.
- */
-function formatLevel(logFormat, level) {
-	let logValue = logFormat;
-	const matcher = StringUtils.match("%([+-]\\d+)?l", logValue);
-
-	if (matcher.isMatch === true) {
-		const levelRegex = matcher.match[0];
-		const levelPadding = matcher.match[1];
-
-		if (levelPadding !== undefined) {
-			const padAmount = levelPadding.substr(1);
-
-			logValue = logValue.replace(
-				levelRegex,
-				levelPadding.substr(0, 1) === "+" ? colorPallet[level.toLowerCase()](level.padEnd(padAmount)) : colorPallet[level.toLowerCase()](level.padStart(padAmount))
-			);
+			logValue = logValue.replace(dateFormatRegex, DateUtils.format(dateFormat, new Date()));
 		}
-		logValue.replace("%s", level);
+		return logValue;
 	}
-	return logValue;
-}
 
-/*
- * Replaces the message placeholder in the passed format.
- *
- * Parameters:
- * logFormat - The log format string to parse.
- * message - The message to inject.
- */
-function formatMessage(logFormat, message) {
-	let logValue = logFormat;
+	/*
+	 * Replaces the log level placeholder in the passed format.
+	 *
+	 * Parameters:
+	 * logFormat - The log format string to parse.
+	 * level - The log level to inject.
+	 */
+	function formatLevel(logFormat, level) {
+		let logValue = logFormat;
+		const matcher = StringUtils.match("%([+-]\\d+)?l", logValue);
 
-	logValue = logValue.replace("%s", message);
-	return logValue;
-}
+		if (matcher.isMatch === true) {
+			const levelRegex = matcher.match[0];
+			const levelPadding = matcher.match[1];
 
-/*
- * Parses the format, replacing placeholders with desired values.
- *
- * Parameters:
- * logFormat - The log format string to parse.
- * logObject - The logObject to inject.
- */
-function format(logFormat, logObject) {
-	let logValue = formatDate(logFormat);
+			if (levelPadding !== undefined) {
+				const padAmount = levelPadding.substr(1);
 
-	logValue = formatLevel(logValue, logObject.level);
-	logValue = formatMessage(logValue, logObject.message);
-	return logValue;
-}
+				logValue = logValue.replace(
+					levelRegex,
+					levelPadding.substr(0, 1) === "+" ? colorPallet[level.toLowerCase()](level.padEnd(padAmount)) : colorPallet[level.toLowerCase()](level.padStart(padAmount))
+				);
+			}
+			logValue.replace("%s", level);
+		}
+		return logValue;
+	}
 
-/*
- * Gets the index of the passed log level.
- *
- * Parameters:
- * level - The level you want the associated index for.
- */
-function logIndex(level) {
-	let index;
+	/*
+	 * Replaces the message placeholder in the passed format.
+	 *
+	 * Parameters:
+	 * logFormat - The log format string to parse.
+	 * message - The message to inject.
+	 */
+	function formatMessage(logFormat, message) {
+		let logValue = logFormat;
 
-	if (level === undefined) {
-		index = logLevelIndex[loggerLogLevel];
+		logValue = logValue.replace("%s", message);
+		return logValue;
+	}
+
+	/*
+	 * Parses the format, replacing placeholders with desired values.
+	 *
+	 * Parameters:
+	 * logFormat - The log format string to parse.
+	 * logObject - The logObject to inject.
+	 */
+	function format(logFormat, logObject) {
+		let logValue = formatDate(logFormat);
+
+		logValue = formatLevel(logValue, logObject.level);
+		logValue = formatMessage(logValue, logObject.message);
+		return logValue;
+	}
+
+	/*
+	 * Gets the index of the passed log level.
+	 *
+	 * Parameters:
+	 * level - The level you want the associated index for.
+	 */
+	function logIndex(level) {
+		return logLevelIndex[level];
+	}
+
+	function isLevelEnabled(level) {
+		return logIndex(loggerLogLevel) <= logIndex(level);
+	}
+
+	/*
+	 * Basic log function, used only internally.
+	 *
+	 * Parameters:
+	 * level - The log level associated with the message.
+	 * object - The log object to log. Accepts string|number|boolean
+	 */
+	function log(level, object) {
+		const printableTypes = ["string", "number", "boolean"];
+		const logObject = {level, message: object};
+
+		if (ArrayUtils.contains(printableTypes, typeof object)) {
+			console.log(
+				format(loggerPattern, logObject)
+			);
+		} else {
+			throw "Cannot log objects of this type.";
+		}
+	}
+
+	const publicMethods = {};
+
+	/*
+	 * For logging DEBUG level messages.
+	 *
+	 * Parameters:
+	 * object - The object to log.
+	 */
+	publicMethods.debug = (object) => {
+		if (isLevelEnabled("DEBUG")) {
+			log("DEBUG", object);
+		}
+	};
+
+	/*
+	 * For logging INFO level messages.
+	 *
+	 * Parameters:
+	 * object - The object to log.
+	 */
+	publicMethods.info = (object) => {
+		if (isLevelEnabled("INFO")) {
+			log("INFO", object);
+		}
+	};
+
+	/*
+	 * For logging WARN level messages.
+	 *
+	 * Parameters:
+	 * object - The object to log.
+	 */
+	publicMethods.warn = (object) => {
+		if (isLevelEnabled("WARN")) {
+			log("WARN", object);
+		}
+	};
+
+	/*
+	 * For logging ERROR level messages.
+	 *
+	 * Parameters:
+	 * object - The object to log.
+	 */
+	publicMethods.error = (object) => {
+		if (isLevelEnabled("ERROR")) {
+			log("ERROR", object);
+		}
+	};
+
+	/*
+	 * Checks to see if the LOG_LEVEL environment variable has been set.
+	 */
+	if (process.env.LOG_LEVEL !== undefined) {
+		const logLevel = process.env.LOG_LEVEL;
+
+		if (ArrayUtils.contains(logLevels, logLevel)) {
+			loggerLogLevel = logLevel;
+		} else {
+			warn(`Log level invalid, defaulting to ${colorPallet[loggerLogLevel.toLowerCase()](loggerLogLevel)}`);
+		}
 	} else {
-		index = logLevelIndex[level];
+		info(`Log level not set, defaulting to ${colorPallet[loggerLogLevel.toLowerCase()](loggerLogLevel)}`);
 	}
-	return index;
-}
 
-/*
- * Basic log function, used only internally.
- *
- * Parameters:
- * level - The log level associated with the message.
- * object - The log object to log. Accepts string|number|boolean
- */
-function log(level, object) {
-	const printableTypes = ["string", "number", "boolean"];
-	const logObject = {level, message: object};
-
-	if (ArrayUtils.contains(printableTypes, typeof object)) {
-		console.log(
-			format(loggerPattern, logObject)
-		);
+	/*
+	 * Checks to see if the LOG_PATTERN environment variable has been set.
+	 */
+	if (typeof process.env.LOG_PATTERN === "string") {
+		loggerPattern = process.env.LOG_PATTERN;
 	} else {
-		throw "Cannot log objects of this type.";
+		info(`Log pattern not set, defaulting to ${loggerPattern}`);
 	}
-}
 
-/*
- * For logging DEBUG level messages.
- *
- * Parameters:
- * object - The object to log.
- */
-function debug(object) {
-	if (logIndex() <= logIndex("DEBUG")) {
-		log("DEBUG", object);
-	}
-}
+	return publicMethods;
+})();
 
-/*
- * For logging INFO level messages.
- *
- * Parameters:
- * object - The object to log.
- */
-function info(object) {
-	if (logIndex() <= logIndex("INFO")) {
-		log("INFO", object);
-	}
-}
-
-/*
- * For logging WARN level messages.
- *
- * Parameters:
- * object - The object to log.
- */
-function warn(object) {
-	if (logIndex() <= logIndex("WARN")) {
-		log("WARN", object);
-	}
-}
-
-/*
- * For logging ERROR level messages.
- *
- * Parameters:
- * object - The object to log.
- */
-function error(object) {
-	if (logIndex() <= logIndex("ERROR")) {
-		log("ERROR", object);
-	}
-}
-
-/*
- * Checks to see if the LOG_LEVEL environment variable has been set.
- */
-if (process.env.LOG_LEVEL !== undefined) {
-	const logLevel = process.env.LOG_LEVEL;
-
-	if (ArrayUtils.contains(logLevels, logLevel)) {
-		loggerLogLevel = logLevel;
-	} else {
-		warn(`Log level invalid, defaulting to ${colorPallet[loggerLogLevel.toLowerCase()](loggerLogLevel)}`);
-	}
-} else {
-	info(`Log level not set, defaulting to ${colorPallet[loggerLogLevel.toLowerCase()](loggerLogLevel)}`);
-}
-
-/*
- * Checks to see if the LOG_PATTERN environment variable has been set.
- */
-if (typeof process.env.LOG_PATTERN === "string") {
-	loggerPattern = process.env.LOG_PATTERN;
-} else {
-	info(`Log pattern not set, defaulting to ${loggerPattern}`);
-}
-
-exports.debug = debug;
-exports.info = info;
-exports.warn = warn;
-exports.error = error;
+exports.debug = Logger.debug;
+exports.info = Logger.info;
+exports.warn = Logger.warn;
+exports.error = Logger.error;
