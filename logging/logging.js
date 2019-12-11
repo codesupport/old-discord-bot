20c44c5e7dbc3ece0177afa80c49e1d851deae55
Atrum <kyle@design-wright.net>
2019-12-10 23:34:18 -0500

    Fix bug with logger formatting

--- logging/logging.js
+++ logging/logging.js
@@ -100,7 +100,7 @@ const Logger = (() => {
 		let logValue = formatDate(logFormat);
 
 		logValue = formatLevel(logValue, logObject.level);
-		logValue = logFormat.replace("%s", message);
+		logValue = logFormat.replace("%s", logObject.message);
 		return logValue;
 	}
 
