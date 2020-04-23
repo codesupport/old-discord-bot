17a0119ad3741e30f3eb659b86ef2f8a2e992f5c
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-04-23 12:33:12 +0100

    Made authentication checks mandatory.

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -10,9 +10,9 @@ function run(member) {
 	const dateDifference = currentDate - memberCreatedDate;
 
 	// Check that the member has an avatar and their account is at least 30 days old.
-	if (member.user.avatar && dateDifference / 2592000000 > 1) {
-		member.addRole(config.roles.member, "Appears to be a valid account.");
-	}
+	//if (member.user.avatar && dateDifference / 2592000000 > 1) {
+	//	member.addRole(config.roles.member, "Appears to be a valid account.");
+	//}
 }
 
 // Export the data
