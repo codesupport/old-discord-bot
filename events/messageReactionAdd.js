9b9e341a7096ab64c9eb65efca9579e5eb8bbc36
Lambo <12111454+LamboCreeper@users.noreply.github.com>
2020-04-23 09:06:34 +0100

    Update messageReactionAdd.js

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -6,7 +6,7 @@ const config = app.config;
 // The code that runs when the event is executed.
 function run(reaction, user) {
 	if (reaction.message.id == config.authentication_message) {
-		if (reaction.emoji.name == "🤖") {
+		if (reaction.emoji.name == "✅") {
 			reaction.message.guild.fetchMember(user).then((member) => {
 				member.addRole(config.roles.member, "User has authenticated their account.");
 			});
